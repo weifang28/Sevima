f1267d2c015d0b71326a0d32d2ff27ecd3c41934
weifang28 <60765003+weifang28@users.noreply.github.com>
2025-04-18 16:23:47 +0700

    Minor bug fix

--- sevima-app/resources/views/find_friends.blade.php
+++ sevima-app/resources/views/find_friends.blade.php
@@ -10,29 +10,35 @@
                     <h5 class="card-title mb-0">{{$user->username}}</h5>
                     <p class="text-muted small">{{"@" . $user->username}}</p>
                     {{-- <p class="fw-bold mb-2">12.7K Followers</p> --}}
-                    @if (count($friends) > 0)
-                        @foreach ($friends as $f)
-                            @if ($user->userid == $f->userid2)
-                                <form action="{{ route('unfollow') }}" method="post">
-                                    @csrf
-                                    <input type="hidden" name="friendid" value="{{$user->userid}}">
-                                    <button class="btn btn-primary btn-sm">Unfollow</button>
-                                </form>
-                            @else
-                                <form action="{{ route('follow') }}" method="post">
-                                    @csrf
-                                    <input type="hidden" name="friendid" value="{{$user->userid}}">
-                                    <button class="btn btn-outline-primary btn-sm">Follow</button>
-                                </form>
-                            @endif
-                        @endforeach
-                    @else
-                        <form action="{{ route('follow') }}" method="post">
-                            @csrf
-                            <input type="hidden" name="friendid" value="{{$user->userid}}">
-                            <button class="btn btn-outline-primary btn-sm">Follow</button>
-                        </form>
-                    @endif
+                    @php
+                    $isFollowing = false;
+                @endphp
+                
+                @if (count($friends) > 0)
+                    @foreach ($friends as $f)
+                        @if ($user->userid == $f->userid2)
+                            @php
+                                $isFollowing = true;
+                                break;
+                            @endphp
+                        @endif
+                    @endforeach
+                @endif
+                
+                @if ($isFollowing)
+                    <form action="{{ route('unfollow') }}" method="post">
+                        @csrf
+                        <input type="hidden" name="friendid" value="{{ $user->userid }}">
+                        <button class="btn btn-primary btn-sm">Unfollow</button>
+                    </form>
+                @else
+                    <form action="{{ route('follow') }}" method="post">
+                        @csrf
+                        <input type="hidden" name="friendid" value="{{ $user->userid }}">
+                        <button class="btn btn-outline-primary btn-sm">Follow</button>
+                    </form>
+                @endif
+                
 
                 </div>
             </div>
